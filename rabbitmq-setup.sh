87d8a6d5c4b5ce0d301f4493f73272f278d862fa
Baba Adeniran <babaadeniran@Babas-MacBook-Pro.local>
2017-08-15 20:32:21 -0600

    Added script for hermes

---
+++ rabbitmq-setup.sh
@@ -0,0 +1,17 @@
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list && sudo apt-get update && sudo apt-get install rabbitmq-server
+sudo apt-get install cmake libboost-dev openssl libssl-dev libblkid-dev e2fslibs-dev libboost-all-dev libaudit-dev
+cd /tmp/
+git clone https://github.com/alanxz/rabbitmq-c
+mkdir rabbitmq-c/build && cd rabbitmq-c/build
+cmake ..
+cmake --build .
+sudo cp librabbitmq/*.a /usr/local/lib/
+sudo cp librabbitmq/*.so* /usr/local/lib/
+cd /tmp/
+git clone https://github.com/alanxz/SimpleAmqpClient
+mkdir SimpleAmqpClient/build && cd SimpleAmqpClient/build
+cmake -DRabbitmqc_INCLUDE_DIR=../../rabbitmq-c/librabbitmq -DRabbitmqc_LIBRARY=../../rabbitmq-c/build/librabbitmq ..
+make
+sudo mkdir /usr/local/include/SimpleAmqpClient
+sudo cp *.so* /usr/local/lib/
+sudo cp ../src/SimpleAmqpClient/*.h /usr/local/include/SimpleAmqpClient
