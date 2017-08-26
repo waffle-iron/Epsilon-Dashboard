3d064cc48e6273d579180af7e34a7900ac11e1de
badenira <baba.adeniran@gmail.com>
2017-08-26 14:55:46 -0600

    Changed checking and shebang

--- EpsilonDashboardSetup.sh
+++ EpsilonDashboardSetup.sh
@@ -1,10 +1,10 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
 echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list && sudo apt-get update && sudo apt-get install rabbitmq-server
 sudo apt-get install cmake libboost-dev openssl libssl-dev libblkid-dev e2fslibs-dev libboost-all-dev libaudit-dev software-properties-common build-essential mesa-common-dev libgl1-mesa-dev
 
 cd /tmp/
-if [ -e /usr/local/librabbitmq.*]
+if [ ls /usr/local/librabbitmq.* 1> /dev/null 2>&1 ];
 then
 echo "Rabbitmq already setup"
 else
@@ -16,9 +16,8 @@ sudo cp librabbitmq/*.a /usr/local/lib/
 sudo cp librabbitmq/*.so* /usr/local/lib/
 fi
 
-
 cd /tmp/
-if [ -e /usr/local/libSimpleAmqpClient.*] || [ -d /usr/local/include/SimpleAmqpClient]
+if [ ls /usr/local/libSimpleAmqpClient.* 1> /dev/null 2>&1 ] || [ -d /usr/local/include/SimpleAmqpClient];
 then
 echo "SimpleAmqpClient already setup"
 else
